3cda2563c0d2fed7e52c02e10106fc11c42c6e44
yechan0616 <mychan611@gmail.com>
2024-06-02 23:16:29 +0900

    test

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -8,6 +8,7 @@ export function GlobalStyle() {
       @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css");
         
       body,
+      
       * {
         font-family:
                 Pretendard,
